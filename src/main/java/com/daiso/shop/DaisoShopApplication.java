b998df9368a2fbc9b25baf479ad2da8fba396624
정현 <iamjhjang@naver.com>
2026-02-24 23:44:19 +0900

    init

--- src/main/java/com/daiso/shop/DaisoShopApplication.java
+++ src/main/java/com/daiso/shop/DaisoShopApplication.java
@@ -2,8 +2,10 @@ package com.daiso.shop;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class DaisoShopApplication {
 
     public static void main(String[] args) {
